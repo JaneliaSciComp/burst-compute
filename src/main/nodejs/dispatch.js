087972ca26d44bacb4cb5bb779a78ba3aa0d295a
Konrad Rokicki <rokickik@janelia.hhmi.org>
2020-10-08 17:16:51 -0400

    reformatted with eslint

--- src/main/nodejs/dispatch.js
+++ src/main/nodejs/dispatch.js
@@ -1,139 +1,174 @@
-'use strict';
-
-const { invokeAsync, startStepFunction } = require('./utils');
 const { v1: uuidv1 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const DEBUG = !!process.env.DEBUG;
+const dispatchFunctionName = process.env.DISPATCH_FUNCTION_ARN;
+const monitorFunctionName = process.env.MONITOR_FUNCTION_ARN;
+const stateMachineName = process.env.STATE_MACHINE_NAME;
 
 const DEFAULTS = {
-    level: 0,
-    numLevels: 2,
-    jobParameters: {},
-    maxParallelism: 3000
+  level: 0,
+  numLevels: 2,
+  jobParameters: {},
+  maxParallelism: 3000,
 };
 
-const dispatchFunctionName = process.env.DISPATCH_FUNCTION_ARN
-const monitorFunctionName = process.env.MONITOR_FUNCTION_ARN
-const stateMachineName = process.env.STATE_MACHINE_NAME
+AWS.config.apiVersions = {
+  lambda: '2015-03-31',
+  s3: '2006-03-01',
+};
+
+const lambda = new AWS.Lambda();
+const stepFunction = new AWS.StepFunctions();
+
+// Invoke another Lambda function asynchronously
+const invokeAsync = async (functionName, parameters) => {
+  if (DEBUG) console.log(`Invoke async ${functionName} with`, parameters);
+  const params = {
+    FunctionName: functionName,
+    InvokeArgs: JSON.stringify(parameters),
+  };
+  try {
+    return await lambda.invokeAsync(params).promise();
+  } catch (e) {
+    console.error('Error invoking', params, e);
+    throw e;
+  }
+};
+
+// Start state machine
+const startStepFunction = async (stateMachineArn, stateMachineParams, uniqueName) => {
+  const params = {
+    stateMachineArn,
+    input: JSON.stringify(stateMachineParams),
+    name: uniqueName,
+  };
+  const result = await stepFunction.startExecution(params).promise();
+  console.log('Step function started: ', result.executionArn);
+  return result;
+};
 
 exports.dispatchHandler = async (event) => {
+  // This next log statement is parsed by the analyzer. DO NOT CHANGE.
+  console.log('Input event:', JSON.stringify(event));
 
+  // User defined parameters
+  const { workerFunctionName } = event;
+  const { reduceFunctionName } = event;
+
+  // Parameters which have defaults
+  const level = parseInt(event.level) || DEFAULTS.level;
+  const numLevels = parseInt(event.numLevels) || DEFAULTS.numLevels;
+  const jobParameters = event.jobParameters || DEFAULTS.jobParameters;
+  const maxParallelism = event.maxParallelism || DEFAULTS.maxParallelism;
+
+  // Programmatic parameters. In the case of the root manager, these will be null initially
+  // and then generated for later invocations.
+  let { jobId } = event;
+  let { monitorName } = event;
+  let batchSize = parseInt(event.batchSize);
+  let numBatches = parseInt(event.numBatches);
+  let branchingFactor = parseInt(event.branchingFactor);
+  const startIndex = parseInt(event.startIndex);
+  const endIndex = parseInt(event.endIndex);
+
+  if (level === 0) {
     // This next log statement is parsed by the analyzer. DO NOT CHANGE.
-    console.log('Input event:', JSON.stringify(event))
+    console.log('Root Dispatcher');
 
-    // User defined parameters
-    const workerFunctionName = event.workerFunctionName
-    const reduceFunctionName = event.reduceFunctionName
-    
-    // Parameters which have defaults
-    const level = parseInt(event.level) || DEFAULTS.level
-    const numLevels = parseInt(event.numLevels) || DEFAULTS.numLevels
-    const jobParameters = event.jobParameters || DEFAULTS.jobParameters
-    const maxParallelism = event.maxParallelism || DEFAULTS.maxParallelism
+    // Generate new job id
+    jobId = uuidv1();
 
-    // Programmatic parameters. In the case of the root manager, these will be null initially and then generated for later invocations.
-    let jobId = event.jobId
-    let monitorName = event.monitorName
-    let batchSize = parseInt(event.batchSize)
-    let numBatches = parseInt(event.numBatches)
-    let branchingFactor = parseInt(event.branchingFactor)
-    let startIndex = parseInt(event.startIndex)
-    let endIndex = parseInt(event.endIndex)
-
-    if (level === 0) {
-        // This next log statement is parsed by the analyzer. DO NOT CHANGE.
-        console.log("Root Dispatcher");
-        
-        // Generate new job id
-        jobId = uuidv1();
-
-        numBatches = Math.ceil(endIndex / batchSize);
-        console.log(`Partition ${endIndex} searches into ${numBatches} of size ${batchSize}`);
-        if (numBatches > maxParallelism) {
-            // adjust the batch size so that we don't exceed the max parallelism requested
-            batchSize = Math.ceil(endIndex / maxParallelism)
-            numBatches = Math.ceil(endIndex / batchSize);
-            console.log(`Capping batch size to ${batchSize} due to max parallelism (${maxParallelism})`);
-        }
-        // the branchingFactor formula assumes that each leaf node at level = <numLevels> corresponds to a batch of size <batchSize>
-        // nLeafNodes = totalSearches / batchSize = branchingFactor ^ numLevels
-        branchingFactor = Math.ceil(Math.pow(numBatches, 1/numLevels)); // e.g. ceil(695^(1/3)) = ceil(8.86) = 9
-
-        // Start monitoring
-        const now = new Date();
-        const monitorParams = {
-            jobId,
-            numBatches,
-            startTime: now.toISOString(),
-            monitorFunctionName, 
-            reduceFunctionName
-        }
-        monitorName = await startMonitor(jobId, stateMachineName, monitorParams, monitorFunctionName);
+    numBatches = Math.ceil(endIndex / batchSize);
+    console.log(`Partition ${endIndex} searches into ${numBatches} of size ${batchSize}`);
+    if (numBatches > maxParallelism) {
+      // adjust the batch size so that we don't exceed the max parallelism requested
+      batchSize = Math.ceil(endIndex / maxParallelism);
+      numBatches = Math.ceil(endIndex / batchSize);
+      console.log(`Capping batch size to ${batchSize} due to max parallelism (${maxParallelism})`);
     }
+    // the branchingFactor formula assumes that each leaf node at level = <numLevels> corresponds
+    // to a batch of size <batchSize>
+    //   nLeafNodes = totalSearches / batchSize = branchingFactor ^ numLevels
+    //   e.g. ceil(695^(1/3)) = ceil(8.86) = 9
+    branchingFactor = Math.ceil(numBatches ** (1 / numLevels));
 
-    // This next log statement is parsed by the analyzer. DO NOT CHANGE.
-    console.log(`Monitor: ${monitorName}`);
-
-    const nextLevelManagerRange = Math.pow(branchingFactor, numLevels-level-1) * batchSize;
-    console.log(`Level ${level} -> next range: ${nextLevelManagerRange}`);
-    const nextEvent = {
-        level: level + 1,
-        numLevels: numLevels,
-        jobId: jobId,
-        jobParameters: jobParameters,
-        batchSize: batchSize,
-        numBatches: numBatches,
-        branchingFactor: branchingFactor,
-        monitorName: monitorName
-    }
-
-    if (level + 1 < numLevels) {
-        // start more intermediate dispatchers
-        for(let i = startIndex; i < endIndex; i += nextLevelManagerRange) {
-            const workerStart = i;
-            const workerEnd = i+nextLevelManagerRange > endIndex ? endIndex : i+nextLevelManagerRange;
-            const params = {
-                startIndex: workerStart,
-                endIndex: workerEnd,
-                ...nextEvent
-            }
-            const invokeResponse = await invokeAsync(dispatchFunctionName, params);
-            console.log(`Dispatched sub-dispatcher ${workerStart} - ${workerEnd} [status=${invokeResponse.Status}]`);
-        }
-    } else {
-        // this is the parent of leaf node (each leaf node corresponds to a batch) so start the batch
-        let batchId = Math.ceil(startIndex / batchSize)
-        for(let i = startIndex; i < endIndex; i += batchSize) {
-            const workerStart = i;
-            const workerEnd = i+batchSize > endIndex ? endIndex : i+batchSize;
-            // This next log statement is parsed by the analyzer. DO NOT CHANGE.
-            console.log(`Dispatching Batch Id: ${batchId}`)
-            const params = {
-                monitorName: monitorName,
-                jobId: jobId,
-                batchId: batchId,
-                jobParameters: jobParameters,
-                startIndex: workerStart,
-                endIndex: workerEnd,
-                ...nextEvent
-            };
-            const invokeResponse = await invokeAsync(workerFunctionName, params);
-            console.log(`Dispatched batch #${batchId} to ${workerFunctionName} [status=${invokeResponse.Status}]`);
-            batchId++;
-        }
-    }
-
-    return {
-        monitorName,
-        jobId
+    // Start monitoring
+    const now = new Date();
+    const monitorParams = {
+      jobId,
+      numBatches,
+      startTime: now.toISOString(),
+      monitorFunctionName,
+      reduceFunctionName,
     };
-}
 
-const startMonitor = async (jobId, stateMachineName, monitorParams) => {
-    await startStepFunction(
+    monitorName = await startStepFunction(stateMachineName, monitorParams, jobId);
+  }
+
+  // This next log statement is parsed by the analyzer. DO NOT CHANGE.
+  console.log(`Monitor: ${monitorName}`);
+
+  const nextLevelManagerRange = (branchingFactor ** (numLevels - level - 1)) * batchSize;
+  console.log(`Level ${level} -> next range: ${nextLevelManagerRange}`);
+  const nextEvent = {
+    level: level + 1,
+    numLevels,
+    jobId,
+    jobParameters,
+    batchSize,
+    numBatches,
+    branchingFactor,
+    monitorName,
+  };
+
+  const invokePromises = [];
+
+  if (level + 1 < numLevels) {
+    // start more intermediate dispatchers
+    let index = 0;
+    for (let i = startIndex; i < endIndex; i += nextLevelManagerRange) {
+      const workerStart = i;
+      const workerEnd = i + nextLevelManagerRange > endIndex ? endIndex : i + nextLevelManagerRange;
+      const params = {
+        startIndex: workerStart,
+        endIndex: workerEnd,
+        ...nextEvent,
+      };
+      invokePromises.push(invokeAsync(dispatchFunctionName, params));
+      console.log(`(Promise#${index}) Dispatched sub-dispatcher ${workerStart} - ${workerEnd}`);
+      index += 1;
+    }
+  } else {
+    // this is the parent of leaf node (each leaf node corresponds to a batch) so start the batch
+    let batchId = Math.ceil(startIndex / batchSize);
+    let index = 0;
+    for (let i = startIndex; i < endIndex; i += batchSize) {
+      const workerStart = i;
+      const workerEnd = i + batchSize > endIndex ? endIndex : i + batchSize;
+      // This next log statement is parsed by the analyzer. DO NOT CHANGE.
+      console.log(`Dispatching Batch Id: ${batchId}`);
+      const params = {
+        monitorName,
         jobId,
-        monitorParams,
-        stateMachineName
-    );
-    return jobId;
-}
+        batchId,
+        jobParameters,
+        startIndex: workerStart,
+        endIndex: workerEnd,
+        ...nextEvent,
+      };
+      invokePromises.push(invokeAsync(workerFunctionName, params));
+      console.log(`(Promise#${index}) Dispatched batch #${batchId} to ${workerFunctionName}`);
+      batchId += 1;
+      index += 1;
+    }
+  }
 
+  const responses = await Promise.all(invokePromises);
+  responses.forEach((r, i) => console.log(`(Promise#${i}) status=${r.Status}`));
 
+  return {
+    monitorName,
+    jobId,
+  };
+};
--- src/main/nodejs/monitor.js
+++ src/main/nodejs/monitor.js
@@ -1,82 +1,79 @@
-'use strict';
+const AWS = require('aws-sdk');
+const moment = require('moment');
 
-const AWS = require('aws-sdk')
-const moment = require('moment')
+const { JOB_TIMEOUT_SECS } = process.env;
+const { JOB_TABLE_NAME } = process.env;
 
-const JOB_TIMEOUT_SECS = process.env.JOB_TIMEOUT_SECS
-const JOB_TABLE_NAME = process.env.JOB_TABLE_NAME
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-var docClient = new AWS.DynamoDB.DocumentClient()
-
-exports.monitorHandler = async (event) =>  {
-    console.log(event);
-    try {
-        return await monitorJob(event);
-    } catch (e) {
-        console.log('Error while checking if job completed', event, e);
-        return {
-            ...event,
-            completed: true,
-            timedOut: false,
-            withErrors: true
-        };
-    }
-}
 const monitorJob = async (jobParams) => {
+  // Parameters
+  const { jobId } = jobParams;
+  const { numBatches } = jobParams;
+  const startTime = moment(jobParams.startTime);
 
-    // Parameters
-    const jobId = jobParams.jobId;
-    const numBatches = jobParams.numBatches;
-    const startTime = moment(jobParams.startTime);
+  // Find out how many tasks are remaining
+  const params = {
+    TableName: JOB_TABLE_NAME,
+    ConsistentRead: true,
+    Select: 'COUNT',
+    KeyConditionExpression: 'jobId = :jobId',
+    ExpressionAttributeValues: {
+      ':jobId': jobId,
+    },
+  };
+  console.log('Fetching result count: ', params);
+  const countResult = await docClient.query(params).promise();
+  const numComplete = countResult.Count;
+  console.log(`Tasks completed: ${numComplete}/${numBatches}`);
 
-    // Find out how many tasks are remaining
-    const params = {
-        TableName: JOB_TABLE_NAME,
-        ConsistentRead: true,
-        Select: "COUNT",
-        KeyConditionExpression: 'jobId = :jobId',
-        ExpressionAttributeValues: {
-            ':jobId': jobId
-        },
+  // Calculate total time
+  const now = new Date();
+  const endTime = moment(now.toISOString());
+  const elapsedSecs = endTime.diff(startTime, 's');
+  const numRemaining = numBatches - numComplete;
+
+  // Return result for next state input
+  if (numRemaining === 0) {
+    console.log(`Job took ${elapsedSecs} seconds`);
+    return {
+      ...jobParams,
+      elapsedSecs,
+      numRemaining: 0,
+      completed: true,
+      timedOut: false,
     };
-    console.log('Fetching result count: ', params)
-    const countResult = await docClient.query(params).promise()
-    const numComplete = countResult.Count
-    console.log(`Tasks completed: ${numComplete}/${numBatches}`);
+  } if (elapsedSecs > JOB_TIMEOUT_SECS) {
+    console.log(`Job timed out after ${elapsedSecs} seconds. Completed ${numComplete} of ${numBatches} tasks.`);
+    return {
+      ...jobParams,
+      elapsedSecs,
+      numRemaining,
+      completed: false,
+      timedOut: true,
+    };
+  }
+  console.log(`Job still running after ${elapsedSecs} seconds. Completed ${numComplete} of ${numBatches} tasks.`);
+  return {
+    ...jobParams,
+    elapsedSecs,
+    numRemaining,
+    completed: false,
+    timedOut: false,
+  };
+};
 
-    // Calculate total time
-    const now = new Date();
-    const endTime = moment(now.toISOString());
-    const elapsedSecs = endTime.diff(startTime, "s");
-    const numRemaining = numBatches - numComplete;
-    
-    // Return result for next state input
-    if (numRemaining === 0) {
-        console.log(`Job took ${elapsedSecs} seconds`);
-        return {
-            ...jobParams,
-            elapsedSecs: elapsedSecs,
-            numRemaining: 0,
-            completed: true,
-            timedOut: false
-        };
-    } else if (elapsedSecs > JOB_TIMEOUT_SECS) {
-        console.log(`Job timed out after ${elapsedSecs} seconds. Completed ${numComplete} of ${numBatches} tasks.`);
-        return {
-            ...jobParams,
-            elapsedSecs: elapsedSecs,
-            numRemaining: numRemaining,
-            completed: false,
-            timedOut: true
-        };
-    } else {
-        console.log(`Job still running after ${elapsedSecs} seconds. Completed ${numComplete} of ${numBatches} tasks.`);
-        return {
-            ...jobParams,
-            elapsedSecs: elapsedSecs,
-            numRemaining: numRemaining,
-            completed: false,
-            timedOut: false
-        };
-    }
-}
+exports.monitorHandler = async (event) => {
+  console.log(event);
+  try {
+    return await monitorJob(event);
+  } catch (e) {
+    console.log('Error while checking if job completed', event, e);
+    return {
+      ...event,
+      completed: true,
+      timedOut: false,
+      withErrors: true,
+    };
+  }
+};
